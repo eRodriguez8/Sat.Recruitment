f0af2b30d33e56ba121084fca38c36f763bf225f
Emanuel Rodriguez <emanuel.rodriguez@coalfire.com>
2021-10-01 12:20:08 -0300

    add email validator test

---
+++ Sat.Recruitment.Test/Models/Helpers/EmailValidatorTest.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using System;
+using FluentAssertions;
+
+using Sat.Recruitment.Models.Helpers;
+
+namespace Sat.Recruitment.Test.Models.Helpers
+{
+    public class EmailValidatorTest
+    {
+        [Theory]
+        [InlineData("email.com")]
+        public void GivenAIncorrectEmail_WhenEmailValidatorIsCall_ShouldThrownEmailException(string email)
+        {
+            Action callingWithAIncorrectValue =
+                () => EmailValidator.ValidateEmail(email);
+
+            callingWithAIncorrectValue
+                .Should()
+                .Throw<FormatException>("The specified string is not in the form required for an e-mail address.");
+        }
+
+        [Theory]
+        [InlineData("email@mail.com")]
+        public void GivenACorrectEmail_WhenEmailValidatorIsCall_ShouldPass(string email)
+        {
+            Action callingWithAIncorrectValue =
+                () => EmailValidator.ValidateEmail(email);
+
+            callingWithAIncorrectValue
+                .Should()
+                .NotThrow();
+        }
+    }
+}
